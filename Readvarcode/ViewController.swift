2c71c986c19deab18ba6e2ae7a74daa345baba9a
marina1017 <mari10172001@yahoo.co.jp>
2017-09-15 22:43:02 +0900

    Initial Commit

---
+++ Readvarcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Readvarcode
+//
+//  Created by 中川万莉奈 on 2017/09/15.
+//  Copyright © 2017年 中川万莉奈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
